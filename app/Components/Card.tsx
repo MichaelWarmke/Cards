54fd83aa16682feaffd3ecf32673668aff6cef85
Mike-AMD <mikew670@gmail.com>
2025-03-11 19:40:41 -0500

    added image assets for the cards

--- app/Components/Card.tsx
+++ app/Components/Card.tsx
@@ -24,25 +24,90 @@ const suitColors = {
     none: 'black'
 };
 
-// Function to get the image source for a specific card
-const getCardImageSource = (suit: CardProps['suit'], rank: CardProps['rank']): ImageSourcePropType | null => {
-    // For the special STOP card
-    if (rank === 'STOP' && suit === 'none') {
-        try {
-            return require('../../assets/images/cards/stop.png');
-        } catch (e) {
-            return null; // Return null if image not found
-        }
-    }
-    
-    // For regular playing cards
-    try {
-        return require(`../../assets/images/cards/${rank}_of_${suit}.png`);
-    } catch (e) {
-        return null; // Return null if image not found
-    }
+// Define the type for the card images mapping
+type CardImagesType = {
+  [suit in CardProps['suit']]: {
+    [rank in CardProps['rank']]?: any;
+  };
 };
 
+// Function to get the image source for a specific card
+const cardImages: CardImagesType = {
+    'hearts': {
+      '2': require('../../assets/images/cards/2_of_hearts.png'),
+      '3': require('../../assets/images/cards/3_of_hearts.png'),
+      '4': require('../../assets/images/cards/4_of_hearts.png'),
+      '5': require('../../assets/images/cards/5_of_hearts.png'),
+      '6': require('../../assets/images/cards/6_of_hearts.png'),
+      '7': require('../../assets/images/cards/7_of_hearts.png'),
+      '8': require('../../assets/images/cards/8_of_hearts.png'),
+      '9': require('../../assets/images/cards/9_of_hearts.png'),
+      '10': require('../../assets/images/cards/10_of_hearts.png'),
+      'J': require('../../assets/images/cards/jack_of_hearts.png'),
+      'Q': require('../../assets/images/cards/queen_of_hearts.png'),
+      'K': require('../../assets/images/cards/king_of_hearts.png'),
+      'A': require('../../assets/images/cards/ace_of_hearts.png'),
+    },
+    'diamonds': {
+      '2': require('../../assets/images/cards/2_of_diamonds.png'),
+      '3': require('../../assets/images/cards/3_of_diamonds.png'),
+      '4': require('../../assets/images/cards/4_of_diamonds.png'),
+      '5': require('../../assets/images/cards/5_of_diamonds.png'),
+      '6': require('../../assets/images/cards/6_of_diamonds.png'),
+      '7': require('../../assets/images/cards/7_of_diamonds.png'),
+      '8': require('../../assets/images/cards/8_of_diamonds.png'),
+      '9': require('../../assets/images/cards/9_of_diamonds.png'),
+      '10': require('../../assets/images/cards/10_of_diamonds.png'),
+      'J': require('../../assets/images/cards/jack_of_diamonds.png'),
+      'Q': require('../../assets/images/cards/queen_of_diamonds.png'),
+      'K': require('../../assets/images/cards/king_of_diamonds.png'),
+      'A': require('../../assets/images/cards/ace_of_diamonds.png'),
+    },
+    'clubs': {
+      '2': require('../../assets/images/cards/2_of_clubs.png'),
+      '3': require('../../assets/images/cards/3_of_clubs.png'),
+      '4': require('../../assets/images/cards/4_of_clubs.png'),
+      '5': require('../../assets/images/cards/5_of_clubs.png'),
+      '6': require('../../assets/images/cards/6_of_clubs.png'),
+      '7': require('../../assets/images/cards/7_of_clubs.png'),
+      '8': require('../../assets/images/cards/8_of_clubs.png'),
+      '9': require('../../assets/images/cards/9_of_clubs.png'),
+      '10': require('../../assets/images/cards/10_of_clubs.png'),
+      'J': require('../../assets/images/cards/jack_of_clubs.png'),
+      'Q': require('../../assets/images/cards/queen_of_clubs.png'),
+      'K': require('../../assets/images/cards/king_of_clubs.png'),
+      'A': require('../../assets/images/cards/ace_of_clubs.png'),
+    },
+    'spades': {
+      '2': require('../../assets/images/cards/2_of_spades.png'),
+      '3': require('../../assets/images/cards/3_of_spades.png'),
+      '4': require('../../assets/images/cards/4_of_spades.png'),
+      '5': require('../../assets/images/cards/5_of_spades.png'),
+      '6': require('../../assets/images/cards/6_of_spades.png'),
+      '7': require('../../assets/images/cards/7_of_spades.png'),
+      '8': require('../../assets/images/cards/8_of_spades.png'),
+      '9': require('../../assets/images/cards/9_of_spades.png'),
+      '10': require('../../assets/images/cards/10_of_spades.png'),
+      'J': require('../../assets/images/cards/jack_of_spades.png'),
+      'Q': require('../../assets/images/cards/queen_of_spades.png'),
+      'K': require('../../assets/images/cards/king_of_spades.png'),
+      'A': require('../../assets/images/cards/ace_of_spades.png'),
+    },
+    'none': {
+      'STOP': require('../../assets/images/cards/stop.png'),
+    }
+  };
+
+  const getCardImageSource = (suit: CardProps['suit'], rank: CardProps['rank']): ImageSourcePropType | null => {
+    
+    try {
+        return cardImages[suit]?.[rank];
+    } catch (e) {
+        return null;
+    }
+  }
+
+  
 // Image for card back
 const getCardBackImage = (): ImageSourcePropType | null => {
     try {
